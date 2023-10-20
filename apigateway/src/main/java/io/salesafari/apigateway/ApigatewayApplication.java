f051bc128d680985bde0e770b4f7a156cdb986b8
TheRealMkadmi <wahib.mkadmi@esprit.tn>
2023-10-21 00:25:05 +0100

    🔧 refactor(MsdeliveryApplication.java): replace @EnableEurekaClient with @EnableDiscoveryClient for better service discovery abstraction 🔧 refactor(pom.xml): remove duplicate eureka client dependency 🔧 refactor(application.properties): update application name and eureka client properties for better service registration and discovery
    The duplicate eureka client dependency in pom.xml was removed to avoid redundancy and potential conflicts. The @EnableEurekaClient annotation was replaced with @EnableDiscoveryClie
    nt in MsdeliveryApplication.java to provide a higher level of abstraction for service discovery. This change makes the application more flexible and adaptable to different service
    discovery implementations. The application.properties file was updated to reflect a more consistent application name and to improve service registration and discovery with the eureka client.

--- apigateway/src/main/java/io/salesafari/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/io/salesafari/apigateway/ApigatewayApplication.java
@@ -2,10 +2,10 @@ package io.salesafari.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApigatewayApplication {
 
     public static void main(String[] args) {
