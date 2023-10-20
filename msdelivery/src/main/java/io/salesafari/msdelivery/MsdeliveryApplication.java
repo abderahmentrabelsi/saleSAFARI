090f37c0a548895c511f4cdceda5f133c90dd3f1
TheRealMkadmi <wahib.mkadmi@esprit.tn>
2023-10-21 00:06:56 +0100

    📦 feat(msdelivery): added delivery microservice

--- msdelivery/src/main/java/io/salesafari/msdelivery/MsdeliveryApplication.java
+++ msdelivery/src/main/java/io/salesafari/msdelivery/MsdeliveryApplication.java
@@ -1,15 +1,16 @@
-package io.salesafari.apigateway;
+package io.salesafari.msdelivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
-public class ApigatewayApplication {
+@EnableEurekaClient
+
+public class MsdeliveryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApigatewayApplication.class, args);
+        SpringApplication.run(MsdeliveryApplication.class, args);
     }
 
 }
---
+++ msdelivery/src/test/java/io/salesafari/msdelivery/MsdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.salesafari.msdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsdeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
