ba546160b127147328107e1847205f6692391dbe
aboud <abderrahmen.trabelsi@esprit.tn>
2023-10-25 18:41:09 +0100

    🚀 Add Product Creation Feature
    - Updated the `ProductCard` component to include a button for adding new products.
    - Integrated the `CreateProductModal` component to allow users to create new products.
    - Implemented the product creation feature, where new products are added to the API using a POST request.
    - Display newly created products in the product list.
    - Imported the `Product` type for type checking.
    - Created a new `CreateProductModal` component for product creation.
    - Enhanced the `CreateProductModal` component to capture product details such as name, description, price, rating, and image URL.
    - Added validation for input fields in the modal.
    - Improved the `ProductCard` component by separating product creation and listing concerns for better code organization.

---
+++ frontend/app/products/_components/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    rating: number;
+    image: string;
+    qty: number;
+    slug: string;
+    brand: string;
+}
