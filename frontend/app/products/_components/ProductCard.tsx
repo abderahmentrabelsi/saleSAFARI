0546ec27ee424d3a8b7eade670f2f9b6095802ad
aboud <abderrahmen.trabelsi@esprit.tn>
2023-10-26 14:48:05 +0100

    bug fix

--- frontend/app/products/_components/ProductCard.tsx
+++ frontend/app/products/_components/ProductCard.tsx
@@ -4,76 +4,78 @@ import { Product } from './product'; // Import the Product type
 import CreateProductModal from './CreateProductModal';
 import ProductModal from '@/app/products/_components/ProductModal'; // Import the CreateProductModal component
 import AddToCartButton from '@/components/cart/addToCart';
+import process from "process";
 
 const ProductCard: React.FC = () => {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [showProductForm, setShowProductForm] = useState(false);
+    const [products, setProducts] = useState<Product[]>([]);
+    const [showProductForm, setShowProductForm] = useState(false);
 
-  useEffect(() => {
-    // Fetch products from your API endpoint
-    fetch('http://ms-market:8082/products')
-      .then((response) => response.json())
-      .then((data: Product[]) => setProducts(data));
-  }, []);
+    const baseUrl = process.env.NEXT_PUBLIC_PRODUCTS_API_URL!;
 
-  const handleAddProductClick = () => {
-    setShowProductForm(true);
-  };
+    useEffect(() => {
+        // Fetch products from your API endpoint
+        fetch("${baseUrl}/products")
+            .then((response) => response.json())
+            .then((data: Product[]) => setProducts(data));
+    }, []);
 
-  const handleProductCreate = (newProduct: Product) => {
-    // Send a POST request to add the new product to your API
-    fetch('http://ms-market:8082/products', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(newProduct)
-    })
-      .then((response) => response.json())
-      .then((createdProduct) => {
-        // Add the newly created product to the state
-        setProducts([...products, createdProduct]);
-      })
-      .catch((error) => {
-        console.error('Error adding product:', error);
-      });
-  };
+    const handleAddProductClick = () => {
+        setShowProductForm(true);
+    };
 
-  return (
-    <div>
-      <div className="text-right mb-4">
-        <Button color="primary" onClick={handleAddProductClick}>
-          Add Product
-        </Button>
-      </div>
-      <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
-        {products.map((product) => (
-          <Card key={product.id} shadow="sm">
-            <Image
-              shadow="sm"
-              radius="lg"
-              width="100%"
-              alt={product.name}
-              className="w-full object-cover h-[300px]"
-              src={product.image}
+    const handleProductCreate = (newProduct: Product) => {
+        // Send a POST request to add the new product to your API
+        fetch("${baseUrl}/products", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newProduct),
+        })
+            .then((response) => response.json())
+            .then((createdProduct) => {
+                // Add the newly created product to the state
+                setProducts([...products, createdProduct]);
+            })
+            .catch((error) => {
+                console.error('Error adding product:', error);
+            });
+    };
+
+    return (
+        <div>
+            <div className="text-right mb-4">
+                <Button color="primary" onClick={handleAddProductClick}>
+                    Add Product
+                </Button>
+            </div>
+            <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
+                {products.map((product) => (
+                    <Card key={product.id} shadow="sm">
+                        <Image
+                            shadow="sm"
+                            radius="lg"
+                            width="100%"
+                            alt={product.name}
+                            className="w-full object-cover h-[300px]"
+                            src={product.image}
+                        />
+                        <CardFooter className="text-small">
+                            <h2>Name: {product.name}</h2>
+                            <p>Price: ${product.price.toFixed(2)}</p>
+                        </CardFooter>
+                        <ProductModal product={product} />
+                    </Card>
+                ))}
+            </div>
+
+            <CreateProductModal
+                isOpen={showProductForm}
+                onClose={() => setShowProductForm(false)}
+                onProductCreate={handleProductCreate}
             />
-            <CardFooter className="text-small">
-              <h2>Name: {product.name}</h2>
-              <p>Price: ${product.price.toFixed(2)}</p>
-              <AddToCartButton product={product} />
-            </CardFooter>
-            <ProductModal product={product} />
-          </Card>
-        ))}
-      </div>
-
-      <CreateProductModal
-        isOpen={showProductForm}
-        onClose={() => setShowProductForm(false)}
-        onProductCreate={handleProductCreate}
-      />
-    </div>
-  );
+        </div>
+    );
 };
 
 export default ProductCard;
