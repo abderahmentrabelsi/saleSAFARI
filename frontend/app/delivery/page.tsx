baec4cad7191031ce6b1244b9b8c7eed521d1783
TheRealMkadmi <wahib.mkadmi@esprit.tn>
2023-10-26 14:00:39 +0100

    🚑

--- frontend/app/delivery/page.tsx
+++ frontend/app/delivery/page.tsx
@@ -2,24 +2,23 @@
 
 import React from 'react';
 import DeliveryForm from './deliveryForm';
-
+import { title } from '@components/primitives';
 
 const DeliveryPage = () => {
   const titleStyle = {
     textAlign: 'center', // Center-align the title
-    fontSize: '36px',   // Increase the font size
-    marginBottom: '24px', // Add margin at the bottom
+    fontSize: '36px', // Increase the font size
+    marginBottom: '24px' // Add margin at the bottom
   };
 
   return (
     <div>
-        <h1 style={titleStyle}>Delivery</h1>
+      <h1 className={title()}>Delivery</h1>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <DeliveryForm /> 
+        <DeliveryForm />
       </div>
     </div>
   );
 };
 
 export default DeliveryPage;
-
--- frontend/app/showDeliveries/page.tsx
+++ frontend/app/showDeliveries/page.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import DeliveryShow from './deliveryShow';
-
+import { title } from '@components/primitives';
 
 const DeliveryShowPage = () => {
   const titleStyle = {
     textAlign: 'center', // Center-align the title
-    fontSize: '36px',   // Increase the font size
-    marginBottom: '24px', // Add margin at the bottom
+    fontSize: '36px', // Increase the font size
+    marginBottom: '24px' // Add margin at the bottom
   };
 
   return (
     <div>
-        <h1 style={titleStyle}>Your Deliveries</h1>
+      <h1 className={title()}>Your Deliveries</h1>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <DeliveryShow /> 
+        <DeliveryShow />
       </div>
     </div>
   );
